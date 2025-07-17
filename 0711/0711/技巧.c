3ecb0a11f320fdf2453c2a97059be06dbb054832
sui <330128955@qq.com>
2025-07-17 22:26:19 +0800

    0717

--- 0711/0711/技巧.c
+++ 0711/0711/技巧.c
@@ -4,7 +4,7 @@ int main()
 {
 	int arr[10] = {0};
 	int i = 0;
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 13; i++)
 	{
 		arr[i] = i + 1;
 	}
---
+++ 0717/0717/指针.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "stdio.h"
+int main()
+{
+	int a = 0;
+	for (a = 0;a <= 13; a++)
+	{
+		int ch[10];
+		ch[a] = a;
+	}
+	return 0;
+}
